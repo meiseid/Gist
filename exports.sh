624a04a5b88d7466f6ae5591f0bb56419d61b173
shiomi <shiomi@www.meiseid.co.jp>
2019-10-05 08:22:04 +0900

    modify.exports.sh

--- exports.sh
+++ exports.sh
@@ -18,3 +18,8 @@ ulimit -S -c unlimited
 if [ -d $HOME/.nodebrew ]; then
  export PATH=$HOME/.nodebrew/current/bin:$PATH
 fi
+alias uncrustify="uncrustify -c $HOME/git/Gist/uncrustify.conf --no-backup"
+function pretidy(){
+ prettier $1 | tidy -config $HOME/git/Gist/tidy.conf > $1.pretidy
+ mv $1.pretidy $1
+}
