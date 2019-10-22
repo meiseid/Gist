891c2bcc8562b1cc20f216f89de5414d28436c62
shiomi <shiomi@www.meiseid.co.jp>
2019-10-22 17:06:45 +0900

    add savemail

---
+++ savemail.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main( void )
+{
+	unsigned char buf[8192];
+	int len;
+
+	fprintf( stdout,"---BEGIN DATA CONTENTS---\n" );
+
+	while( 1 ){
+
+		len = fread( buf,1,sizeof(buf),stdin );
+		if( len <= 0 ){
+			break;
+		}
+
+		fwrite( buf,len,1,stdout );
+	}
+
+	fprintf( stdout,"---END DATA CONTENTS---\n" );
+
+	return 0;
+}
+
