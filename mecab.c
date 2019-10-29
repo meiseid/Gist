808764663f595659f18b3d8917fde96818ece6aa
shiomi <shiomi@www.meiseid.co.jp>
2019-10-29 13:13:12 +0900

    modify mecab

--- mecab.c
+++ mecab.c
@@ -8,12 +8,24 @@
 
 int main(void)
 {
-	char  name[512], * ptr;
-	char* argv[] = { (char*) "mecab" }; mecab_t* mecab;
+	char	 name[512], * ptr;
+	char*	 argv_a[] = { (char*) "mecab", (char*) "-Owakati" };
+	char*	 argv_b[] = { (char*) "mecab", (char*) "-Ochasen" };
+	mecab_t* mecab;
 
 	sprintf(name, "すもももももももものうち");
 
-	if((mecab = mecab_new(countof(argv), argv)) != NULL)
+	if((mecab = mecab_new(countof(argv_a), argv_a)) != NULL)
+	{
+		fprintf(stdout, "mecab_new ok\n");
+		if((ptr = (char*) mecab_sparse_tostr(mecab, name)) != NULL)
+		{
+			fprintf(stdout, "mecab_sparse_tostr ok string is:\n%s\n", ptr);
+		}
+		mecab_destroy(mecab);
+	}
+
+	if((mecab = mecab_new(countof(argv_b), argv_b)) != NULL)
 	{
 		fprintf(stdout, "mecab_new ok\n");
 		if((ptr = (char*) mecab_sparse_tostr(mecab, name)) != NULL)
