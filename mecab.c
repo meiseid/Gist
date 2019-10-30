24fa4c854c0879f61d249db0423958e5188f0f23
shiomi <shiomi@www.meiseid.co.jp>
2019-10-30 13:06:12 +0900

    uncrustify config change and reformat

--- mecab.c
+++ mecab.c
@@ -8,32 +8,32 @@
 
 int main(void)
 {
-	char	 name[512], * ptr;
-	char*	 argv_a[] = { (char*) "mecab", (char*) "-Owakati" };
-	char*	 argv_b[] = { (char*) "mecab", (char*) "-Ochasen" };
-	mecab_t* mecab;
+    char name[512], *ptr;
+    char *argv_a[] = { (char*)"mecab", (char*)"-Owakati" };
+    char *argv_b[] = { (char*)"mecab", (char*)"-Ochasen" };
+    mecab_t *mecab;
 
-	sprintf(name, "すもももももももものうち");
+    sprintf(name, "すもももももももものうち");
 
-	if((mecab = mecab_new(countof(argv_a), argv_a)) != NULL)
-	{
-		fprintf(stdout, "mecab_new ok\n");
-		if((ptr = (char*) mecab_sparse_tostr(mecab, name)) != NULL)
-		{
-			fprintf(stdout, "mecab_sparse_tostr ok string is:\n%s\n", ptr);
-		}
-		mecab_destroy(mecab);
-	}
+    if((mecab = mecab_new(countof(argv_a), argv_a)) != NULL)
+    {
+        fprintf(stdout, "mecab_new ok\n");
+        if((ptr = (char*)mecab_sparse_tostr(mecab, name)) != NULL)
+        {
+            fprintf(stdout, "mecab_sparse_tostr ok string is:\n%s\n", ptr);
+        }
+        mecab_destroy(mecab);
+    }
 
-	if((mecab = mecab_new(countof(argv_b), argv_b)) != NULL)
-	{
-		fprintf(stdout, "mecab_new ok\n");
-		if((ptr = (char*) mecab_sparse_tostr(mecab, name)) != NULL)
-		{
-			fprintf(stdout, "mecab_sparse_tostr ok string is:\n%s\n", ptr);
-		}
-		mecab_destroy(mecab);
-	}
+    if((mecab = mecab_new(countof(argv_b), argv_b)) != NULL)
+    {
+        fprintf(stdout, "mecab_new ok\n");
+        if((ptr = (char*)mecab_sparse_tostr(mecab, name)) != NULL)
+        {
+            fprintf(stdout, "mecab_sparse_tostr ok string is:\n%s\n", ptr);
+        }
+        mecab_destroy(mecab);
+    }
 
-	return 0;
+    return 0;
 }
--- savemail.c
+++ savemail.c
@@ -4,23 +4,23 @@
 
 int main(void)
 {
-	unsigned char buf[8192];
-	int len;
+    unsigned char buf[8192];
+    int len;
 
-	fprintf(stdout, "---BEGIN DATA CONTENTS---\n");
+    fprintf(stdout, "---BEGIN DATA CONTENTS---\n");
 
-	while(1)
-	{
-		len = fread(buf, 1, sizeof(buf), stdin);
-		if(len <= 0)
-		{
-			break;
-		}
+    while(1)
+    {
+        len = fread(buf, 1, sizeof(buf), stdin);
+        if(len <= 0)
+        {
+            break;
+        }
 
-		fwrite(buf, len, 1, stdout);
-	}
+        fwrite(buf, len, 1, stdout);
+    }
 
-	fprintf(stdout, "---END DATA CONTENTS---\n");
+    fprintf(stdout, "---END DATA CONTENTS---\n");
 
-	return 0;
+    return 0;
 }
